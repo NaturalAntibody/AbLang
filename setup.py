6497ae9bb710a2b0992198686d6ba266b8b36d0c
Piotr Deszyński <pdeszynski@gmail.com>
2023-02-14 21:27:27 +0100

    chore(deps): udpated torch on arm to 1.13

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setup(
         'fairseq',
         'torch>=1.6; platform_machine != "aarch64"',
         'torch==1.12.0; platform_machine == "aarch64"',
-        'torch>=1.12.0; platform_machine == "arm64"',
+        'torch>=1.13.0; platform_machine == "arm64"',
     ],
 )
